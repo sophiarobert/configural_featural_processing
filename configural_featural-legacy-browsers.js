b9c482c9088a951867188a19de2ec4ba9546102d
Sophia Robert <sophia.r.robert@gmail.com>
2020-10-24 22:34:10 -0400

    duplicated fxn

--- configural_featural-legacy-browsers.js
+++ configural_featural-legacy-browsers.js
@@ -700,6 +700,19 @@ function instrBlock1RoutineBegin(snapshot) {
                     // otherwise we return s:
                     return s;
             }
+    function shuffle_array(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    
+                // swap elements array[i] and array[j]
+                // we use "destructuring assignment" syntax to achieve that
+                // you'll find more details about that syntax in later chapters
+                // same can be written as:
+                // let t = array[i]; array[i] = array[j]; array[j] = t
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array
+    }
     
     if ((Block_type === "conf_face")) {
         paths = ["Stimuli/edmd.png", "Stimuli/eimd.png", "Stimuli/eomu.png", "Stimuli/eumu.png"];
