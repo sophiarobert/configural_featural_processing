5a114693b1fe2940243e4c76d96e959e3248863f
Sophia Robert <sophia.r.robert@gmail.com>
2020-10-28 18:18:49 -0400

    try and get text

--- configural_featural.js
+++ configural_featural.js
@@ -423,7 +423,7 @@ function experimentInit() {
     text: 'default text',
     font: 'Arial',
     units: undefined, 
-    pos: [0, (- 1.5)], height: 0.05,  wrapWidth: undefined, ori: 0,
+    pos: [0, (- 0.5)], height: 0.05,  wrapWidth: undefined, ori: 0,
     color: new util.Color('black'),  opacity: 1,
     depth: -2.0 
   });
