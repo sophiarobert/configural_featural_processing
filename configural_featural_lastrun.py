fad22a50b98a178757f85183c03f16d5f4f584cd
Sophia Robert <sophia.r.robert@gmail.com>
2020-11-05 21:28:23 -0500

    y img no work

--- configural_featural_lastrun.py
+++ configural_featural_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.4),
-    on Thu Nov  5 17:54:06 2020
+    on Thu Nov  5 20:12:31 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
