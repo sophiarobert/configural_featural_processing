f21230f7830c2db182f9bd052e1ff57128ae2cff
Sophia Robert <sophia.r.robert@gmail.com>
2020-11-05 22:42:51 -0500

    stim change again

--- configural_featural_lastrun.py
+++ configural_featural_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.4),
-    on Thu Nov  5 20:12:31 2020
+    on Thu Nov  5 21:29:15 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
