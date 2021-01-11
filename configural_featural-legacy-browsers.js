1b3b6e8c4462a33404db831011bc2a36643025cf
Sophia Robert <sophia.r.robert@gmail.com>
2021-01-11 12:15:02 +0700

    fix block count

--- configural_featural-legacy-browsers.js
+++ configural_featural-legacy-browsers.js
@@ -85,9 +85,9 @@ psychoJS.start({
   expName: expName,
   expInfo: expInfo,
   resources: [
-    {'name': 'bankcard.png', 'path': 'bankcard.png'},
     {'name': 'Designs/smith_sisters.png', 'path': 'Designs/smith_sisters.png'},
-    {'name': 'Designs/smith_houses.png', 'path': 'Designs/smith_houses.png'}
+    {'name': 'Designs/smith_houses.png', 'path': 'Designs/smith_houses.png'},
+    {'name': 'bankcard.png', 'path': 'bankcard.png'}
   ]
 });
 
